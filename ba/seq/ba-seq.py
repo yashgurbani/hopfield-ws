69dde7a2b6e699a69260a5fbe3ddcdf9b8b26885
yashgurbani <geekbuddy4@gmail.com>
2020-06-02 00:43:02 +0530

    new code for hopfield phase diagram

---
+++ ba/seq/ba-seq.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: yash
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+from random import choice
+from matplotlib import cm
+import datetime
+from tqdm import tqdm
+from random import randrange
+import random
+
+div = 10
+n = int(input('Enter # of neurons'))
+m = int(input('Enter # of memory states'))
+flip = round(0.25 * n)
+
+MemoryMatrix = np.zeros((m, n))
+target_list = []
+
+for a in range(0, m):
+    mem = []
+    for i in range(0, n):
+        x = choice([1, -1])
+        mem.append(x)
+    target_list.append(a)
+    MemoryMatrix[a] = mem
+    x = 0
+
+n_i = 10000 #int(input('Enter # of iterations each run'))
+ensembleCount = int(input('Enter # of runs to average over'))
+#target = int(input('Select memory state which when added noise gives initial state'))
+
+suffix = datetime.datetime.now().strftime("%m%d_%H%M%S")
+filename = "_".join([str(n), str(m), str(n_i), str(ensembleCount), suffix])
+np.savetxt(filename + "-ms-s.csv", np.column_stack((np.transpose(MemoryMatrix))), delimiter=",", fmt='%s')
+
+OverlapMatrixn = np.zeros((ensembleCount, 50))  # initiate overlap matrix
+
+for b in tqdm(range(ensembleCount)):
+    target = random.choice(target_list)
+    u = []
+    u = MemoryMatrix[target].copy()  # copy target M to initial state
+    hammingtemp = overlaptemp = 0
+    hammingavg = overlapavg = 0
+    Y = []
+    X = []
+
+    p = 0
+
+    n_init = random.randint(0, (n - flip))  # start at a random position in the initial state
+    n_fin = n_init + flip
+
+    for count in range(n_init, n_fin):  # sequentially pick up 25% of bits and flip them
+        u[count] = (MemoryMatrix[target][count] * -1)
+
+    for k in (range(1,51)):  # for this given random matrix, loop over different p values
+
+        hammingtemp = overlaptemp = 0
+        hammingval = overlapval = 0
+
+        g = nx.barabasi_albert_graph(n, k, seed=None)
+
+        # set the initial state
+        st = 0
+        for st in range(0, n):
+            g.nodes[st]['state'] = u[st]
+
+        # nx.draw(g, pos = g.pos, cmap = cm.jet, vmin = -1, vmax = 1, node_color = [g.nodes[i]['state'] for i in
+        # g.nodes]) plt.show() print ("Initial Graph")
+
+        # train the network
+        for i, j in g.edges:
+            weight = 0
+            alpha = 0
+            for alpha in range(0, m):
+                weight = weight + (MemoryMatrix[alpha][i] * MemoryMatrix[alpha][j])
+            g.edges[i, j]['weight'] = (weight / n)
+
+        # evolve according to hopfield dynamics, n_i iterations
+        for z in range(0, n_i):
+            i = choice(list(g.nodes))
+            s = sum([g.edges[i, j]['weight'] * g.nodes[j]['state'] for j in g.neighbors(i)])
+            g.nodes[i]['state'] = 1 if s > 0 else -1 if s < 0 else g.nodes[i]['state']
+
+            # nx.draw(g, pos = g.pos, cmap = cm.jet, vmin = -1, vmax = 1, node_color = [g.nodes[i]['state'] for i in
+            # g.nodes]) plt.show()
+            z = z + 1
+
+        # calculate hamming distances and overlap for all the memory states
+
+        for i in list(g.nodes):
+            overlaptemp += (MemoryMatrix[target][i] * g.nodes[i]['state'])
+
+        overlapval = (overlaptemp / n)
+
+        X.append(k)
+        Y.append(overlapval)
+
+    OverlapMatrixn[b] = Y
+
+    b = b + 1
+
+col_totalsO = [sum(x) for x in zip(*OverlapMatrixn)]
+col_totalsOavg = [(x / ensembleCount) for x in col_totalsO]
+
+# set up figure and axes
+plt.plot(X, col_totalsOavg, color="magenta")
+plt.title("\n Variation of Network Performance with Degree Distribution \n %s neruons with %s memory states \n "
+          "Iterations = %s | Ensemble Count= ""%s \n \n" % (n, m, n_i, ensembleCount))
+plt.ylabel("average quality q")
+
+
+suffix = datetime.datetime.now().strftime("%m%d_%H%M%S")
+filename = "_".join([str(n), str(m), str(k), str(n_i), str(ensembleCount), suffix])
+plt.autoscale()
+plt.savefig(filename, dpi=200, bbox_inches = "tight")
+plt.show()
+
+np.savetxt(filename + "-s-O.csv", np.column_stack((X, col_totalsOavg)), delimiter=",", fmt='%s')
---
+++ phase-diagram/normal/phase-hopfield.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: yash
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+from random import choice
+import datetime
+from tqdm import tqdm
+import random
+
+n = int(input('Enter # of neurons'))
+m_f = round(0.2 * n)
+MemoryMatrix = np.zeros((m_f, n))
+
+for a in range(0, m_f):
+    mem = []
+    for i in range(0, n):
+        x = choice([1, -1])
+        mem.append(x)
+    MemoryMatrix[a] = mem
+    x = 0
+
+flip = round(0.3 * n)
+n_i = 10000
+steps = int(input('Enter # of steps in iteration of m (resolution)'))
+
+ErrorMatrixn = np.zeros((1, round((m_f-1)/2)))  # initiate quality matrix
+
+Y = []
+X = []
+u = []
+u = MemoryMatrix[1].copy()  # copy target M to initial state
+rs = random.sample(range(0, n), flip)
+
+for z in list(rs):  # randomly pick up 25 percent and flip them
+    u[z] = (MemoryMatrix[1][z] * -1)
+
+g = nx.complete_graph(n)
+
+st = 0
+for st in range(0, n):
+    g.nodes[st]['state'] = u[st]
+
+for m in tqdm(range (2, (m_f+1), steps)):  # for this given random matrix, loop over different m values
+    alpha = (m/n)
+
+    # train the network with m memory states
+    for i, j in g.edges:
+        weight = 0
+        for zeta in range(0, m):
+            weight = weight + (MemoryMatrix[zeta][i] * MemoryMatrix[zeta][j])
+        g.edges[i, j]['weight'] = (weight / n)
+
+    # evolve according to hopfield dynamics, n_i iterations
+    for z in range(0, n_i):
+        i = choice(list(g.nodes))
+        s = sum([g.edges[i, j]['weight'] * g.nodes[j]['state'] for j in g.neighbors(i)])
+        g.nodes[i]['state'] = 1 if s > 0 else -1 if s < 0 else g.nodes[i]['state']
+        z = z + 1
+
+    # calculate hamming distances
+    hammingtemp = 0
+    for i in list(g.nodes):
+        hammingtemp += abs(g.nodes[i]['state'] - MemoryMatrix[1][i])
+
+    X.append(alpha)
+    Y.append(hammingtemp)
+
+
+col_totalsEavg = [(x / (n * 2)) for x in Y]
+
+# set up figure and axes
+plt.plot(X, col_totalsEavg, color="magenta")
+plt.ylabel("% of errors")
+plt.xlabel("alpha")
+png = ".png"
+suffix = datetime.datetime.now().strftime("%m%d_%H%M%S")
+filename = "_".join([str(n), suffix])
+plt.autoscale()
+plt.savefig(filename, dpi=200, bbox_inches = "tight")
+plt.show()
+
+np.savetxt(filename + "-s-Q.csv", np.column_stack((X, col_totalsEavg)), delimiter=",", fmt='%s')
